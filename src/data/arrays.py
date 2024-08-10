72718747d99d754c4708d47a605970ee2575533f
Juan.Ferreira <juan.ferreira@impar.com.br>
2024-08-10 16:05:03 -0300

    feat: add arrays for selects components

---
+++ src/data/arrays.py
@@ -0,0 +1,15 @@
+companies = ['ACME Corp', 'Globex Corporation', 'Soylent Corp', 'Initech', 'Umbrella Corporation']
+
+quarters = ['Q1', 'Q2', 'Q3', 'Q4']
+
+years = [2021, 2022, 2023, 2024]
+
+languages = ['Português', 'Inglês', 'Espanhol', 'Francês', 'Alemão']
+
+analysis = [
+    "Análise do Balanço Patrimonial",
+    "Análise do Fluxo de Caixa",
+    "Análise de Tendências",
+    "Análise de Receita e Lucro",
+    "Análise de Posição de Mercado"
+]
